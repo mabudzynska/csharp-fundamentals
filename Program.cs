a9359713be892e522323b0817e23348568cb63a3
mabudzynska <mabudzynska00@gmail.com>
2026-03-09 16:22:17 +0100

    initial C# console project

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+
+            Console.WriteLine(number);
+        }
+    }
+}
